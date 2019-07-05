b1a77953d97d7f9ee9244a7760ad02c33d0f6897
lukaszdyszy <MrUhol123@gmail.com>
2019-07-05 10:46:51 +0200

    v1.0

--- src/router.js
+++ src/router.js
@@ -8,7 +8,6 @@ import Archives from "./views/Archives.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   routes: [
     {
       path: "/",
--- src/views/Archives.vue
+++ src/views/Archives.vue
@@ -2,8 +2,10 @@
     <div class="archives">
         <TopBar :mode="'static'"></TopBar>
         <main>
-            <div class="lds-ring" v-if="$apollo.queries.posts.loading"><div></div><div></div><div></div><div></div></div>
             <div class="container">
+                <div class="loading" v-if="$apollo.loading != 0">
+                    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+                </div>
                 <Wall :posts="posts"></Wall>
             </div>
             <div class="pagination">
@@ -121,6 +123,70 @@ main{
     padding: 7px;
     border: 1px solid black;
 }
+
+
+
+.loading{
+    display: flex;
+    justify-content: center;
+}
+.lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 27px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background: rgb(58, 58, 58);
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 6px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 6px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 26px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 45px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(19px, 0);
+  }
+}
+
 
 </style>
 
